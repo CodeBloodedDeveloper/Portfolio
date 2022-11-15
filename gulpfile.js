85453edddf2f9f63b51fd46bfb1e3faeb3823753
Harshil Awasthi <96411822+CodeBloodedDeveloper@users.noreply.github.com>
2022-11-15 22:02:30 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,16 @@ const autoprefixer  = require('gulp-autoprefixer');
 const jpgRecompress = require('imagemin-jpeg-recompress'); 
 const clean         = require('gulp-clean');
 
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
+
 
 // Paths
 var paths = {
